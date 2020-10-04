9e6cb911df10c321c0cdf6704348649e9d5efffc
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-04 21:13:58 +0300

    fix: default values for passed vars, safer handling of value

--- caesar-cipher-cli/src/cipher.js
+++ caesar-cipher-cli/src/cipher.js
@@ -1,4 +1,4 @@
-exports.caesar = function caesar(value, act, input = process.stdout, output = process.stdint) {
+exports.caesar = function caesar(value, act, input = process.stdin, output = process.stdout) {
   if (value < 0) {
     console.error('input a non-negative value for shifting')
     return
@@ -7,24 +7,24 @@ exports.caesar = function caesar(value, act, input = process.stdout, output = pr
     if (char.codePointAt(0) > 96 && char.codePointAt(0) < 123) {
       if (act === 'encode') {
         output.push(String.fromCodePoint(
-          (char.codePointAt(0) + value - 97) % 26 + 97
+          (char.codePointAt(0) + +value - 97) % 26 + 97
           ))
         return
       }
       else output.push(String.fromCodePoint(
-        (char.codePointAt(0) - value - 97) % 26 + 97
+        (char.codePointAt(0) - +value - 97) % 26 + 97
         ))
       return
     }
     if (char.codePointAt(0) > 64 && char.codePointAt(0) < 91) {
       if (act === 'encode') {
         output.push(String.fromCodePoint(
-          (char.codePointAt(0) + value - 65) % 26 + 65
+          (char.codePointAt(0) + +value - 65) % 26 + 65
           ))
         return
       }
       else output.push(String.fromCodePoint(
-        (char.codePointAt(0) - value - 65) % 26 + 65
+        (char.codePointAt(0) - +value - 65) % 26 + 65
         ))
       return
     }
