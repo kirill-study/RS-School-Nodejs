1323f691b94e84dd4a569dac2b77dfca967cbd29
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-04 20:50:38 +0300

    feat: initial version of cipher function, only encode

---
+++ caesar-cipher-cli/src/cipher.js
@@ -0,0 +1,34 @@
+exports.caesar = function caesar(value, act, input = process.stdout, output = process.stdint) {
+  if (value < 0) {
+    console.error('input a non-negative value for shifting')
+    return
+  }
+  input.split('').forEach(char => {
+    if (char.codePointAt(0) > 96 && char.codePointAt(0) < 123) {
+      if (act === 'encode') {
+        output.push(String.fromCodePoint(
+          (char.codePointAt(0) + value - 97) % 26 + 97
+          ))
+        return
+      }
+      else output.push(String.fromCodePoint(
+        (char.codePointAt(0) - value - 97) % 26 + 97
+        ))
+      return
+    }
+    if (char.codePointAt(0) > 64 && char.codePointAt(0) < 91) {
+      if (act === 'encode') {
+        output.push(String.fromCodePoint(
+          (char.codePointAt(0) + value - 65) % 26 + 65
+          ))
+        return
+      }
+      else output.push(String.fromCodePoint(
+        (char.codePointAt(0) - value - 65) % 26 + 65
+        ))
+      return
+    }
+    output.push(char)
+    return
+  })
+}
