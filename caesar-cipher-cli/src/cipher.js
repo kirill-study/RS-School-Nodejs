101b40f9790ecd4d9b40560dbae665c196640fb5
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-05 14:13:27 +0300

    feat: cipher function support for decode

--- caesar-cipher-cli/src/cipher.js
+++ caesar-cipher-cli/src/cipher.js
@@ -1,8 +1,4 @@
 exports.caesar = function caesar(value, act, input = process.stdin, output = process.stdout) {
-  if (value < 0) {
-    console.error('input a non-negative value for shifting')
-    return
-  }
   input.split('').forEach(char => {
     if (char.codePointAt(0) > 96 && char.codePointAt(0) < 123) {
       if (act === 'encode') {
@@ -12,7 +8,7 @@ exports.caesar = function caesar(value, act, input = process.stdin, output = pro
         return
       }
       else output.push(String.fromCodePoint(
-        (char.codePointAt(0) - +value - 97) % 26 + 97
+        (char.codePointAt(0) + 26 - (+value % 26) - 97) % 26 + 97
         ))
       return
     }
@@ -24,11 +20,12 @@ exports.caesar = function caesar(value, act, input = process.stdin, output = pro
         return
       }
       else output.push(String.fromCodePoint(
-        (char.codePointAt(0) - +value - 65) % 26 + 65
+        (char.codePointAt(0) + 26 - (+value % 26) - 65) % 26 + 65
         ))
       return
     }
     output.push(char)
     return
   })
+  //TODO: transform array to string?
 }
