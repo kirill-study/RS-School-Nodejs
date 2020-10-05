5007825c63e8bc7d23bc1668bd749f1c595378e1
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-05 18:21:25 +0300

    feat: initial index.js with piping working in basic cases

--- caesar-cipher-cli/src/index.js
+++ caesar-cipher-cli/src/index.js
@@ -1,1 +1,24 @@
 const { program } = require('commander')
+const { parse } = require('./parse-args.js')
+const { caesar } = require('./cipher.js')
+
+const through2 = require('through2')
+const { pipeline } = require('stream')
+const fs = require('fs')
+
+let inputStream
+let outputStream
+
+parse()
+
+if (program.input === 'undefined') inputStream = fs.createReadStream(process.stdin, "utf8")
+else readStream = fs.createReadStream(program.input, "utf8")
+
+if (program.output === 'undefined') outputStream = fs.createWriteStream(process.stdout)
+else writeStream = fs.createWriteStream(program.output, {flags: 'a'})
+
+const theFunction = through2((data, enc, cb) => {
+  cb(null, Buffer.from(caesar(data.toString())))
+})
+
+readStream.pipe(theFunction).pipe(writeStream)
