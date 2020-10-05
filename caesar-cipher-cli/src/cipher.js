0afa12afce510e41e9a017e11bd969ef1c3d2a03
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-05 18:08:58 +0300

    feat: modify cipher function to be usable in pipes

--- caesar-cipher-cli/src/cipher.js
+++ caesar-cipher-cli/src/cipher.js
@@ -1,45 +1,51 @@
-exports.caesar = function caesar(value, act, input = process.stdin, output = process.stdout) {
-  input.split('').forEach(char => {
+const { parse } = require('./parse-args.js')
+const { program } = require('commander')
+
+exports.caesar = function caesar(input) {
+  let value = program.shift
+  let act = program.action
+  let result = []
+  input.toString().split('').forEach(char => {
     if (char.codePointAt(0) > 96 && char.codePointAt(0) < 123) {
       if (act === 'encode' && value >= 0) {
-        output.push(String.fromCodePoint(
+        result.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 97) % 26 + 97
           ))
         return
       }
       if (act === 'encode' && value < 0) {
         value = 26 + (+value % 26)
-        output.push(String.fromCodePoint(
+        result.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 97) % 26 + 97
           ))
         return
       }
-      else output.push(String.fromCodePoint(
+      else result.push(String.fromCodePoint(
         (char.codePointAt(0) + 26 - (+value % 26) - 97) % 26 + 97
         ))
       return
     }
     if (char.codePointAt(0) > 64 && char.codePointAt(0) < 91) {
       if (act === 'encode' && value >= 0) {
-        output.push(String.fromCodePoint(
+        result.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 65) % 26 + 65
           ))
         return
       }
       if (act === 'encode' && value < 0) {
         value = 26 + (+value % 26)
-        output.push(String.fromCodePoint(
+        result.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 65) % 26 + 65
           ))
         return
       }
-      else output.push(String.fromCodePoint(
+      else result.push(String.fromCodePoint(
         (char.codePointAt(0) + 26 - (+value % 26) - 65) % 26 + 65
         ))
       return
     }
-    output.push(char)
+    result.push(char)
     return
   })
-  //TODO: transform array to string?
+  return result.join('')
 }
