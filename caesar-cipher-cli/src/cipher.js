2482d8cb8b4111ece7e4607f8d4ba2e18fdc8497
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-05 14:29:29 +0300

    feat: cipher function handle negative numbers

--- caesar-cipher-cli/src/cipher.js
+++ caesar-cipher-cli/src/cipher.js
@@ -1,7 +1,14 @@
 exports.caesar = function caesar(value, act, input = process.stdin, output = process.stdout) {
   input.split('').forEach(char => {
     if (char.codePointAt(0) > 96 && char.codePointAt(0) < 123) {
-      if (act === 'encode') {
+      if (act === 'encode' && value >= 0) {
+        output.push(String.fromCodePoint(
+          (char.codePointAt(0) + +value - 97) % 26 + 97
+          ))
+        return
+      }
+      if (act === 'encode' && value < 0) {
+        value = 26 + (+value % 26)
         output.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 97) % 26 + 97
           ))
@@ -13,7 +20,14 @@ exports.caesar = function caesar(value, act, input = process.stdin, output = pro
       return
     }
     if (char.codePointAt(0) > 64 && char.codePointAt(0) < 91) {
-      if (act === 'encode') {
+      if (act === 'encode' && value >= 0) {
+        output.push(String.fromCodePoint(
+          (char.codePointAt(0) + +value - 65) % 26 + 65
+          ))
+        return
+      }
+      if (act === 'encode' && value < 0) {
+        value = 26 + (+value % 26)
         output.push(String.fromCodePoint(
           (char.codePointAt(0) + +value - 65) % 26 + 65
           ))
